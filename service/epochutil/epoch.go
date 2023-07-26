cec5161e2d3dedce46422900b8bc2fb77c0fceaf
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2023-07-26 01:17:18 -0700

    lint

--- service/epochutil/epoch.go
+++ service/epochutil/epoch.go
@@ -10,7 +10,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-var GENESIS_TIMESTAMP = int32(1598306400)
+var GenesisTimestamp = int32(1598306400)
 
 type result struct {
 	Blocks []block
@@ -36,18 +36,18 @@ func Initialize(ctx context.Context, lotusAPI string, lotusToken string) error {
 		return errors.New("length of blocks from genesis is 0")
 	}
 
-	GENESIS_TIMESTAMP = r.Blocks[0].Timestamp
+	GenesisTimestamp = r.Blocks[0].Timestamp
 	return nil
 }
 
 func EpochToTime(epoch int32) time.Time {
-	return time.Unix(int64(epoch)*30+int64(GENESIS_TIMESTAMP), 0)
+	return time.Unix(int64(epoch)*30+int64(GenesisTimestamp), 0)
 }
 
 func UnixToEpoch(unix int64) int32 {
-	return (int32(unix) - GENESIS_TIMESTAMP) / 30
+	return (int32(unix) - GenesisTimestamp) / 30
 }
 
 func TimeToEpoch(t time.Time) abi.ChainEpoch {
-	return abi.ChainEpoch((t.Unix() - int64(GENESIS_TIMESTAMP)) / 30)
+	return abi.ChainEpoch((t.Unix() - int64(GenesisTimestamp)) / 30)
 }
--- service/epochutil/epoch_test.go
+++ service/epochutil/epoch_test.go
@@ -10,11 +10,11 @@ import (
 func TestDefaultValue(t *testing.T) {
 	err := Initialize(context.Background(), "https://api.node.glif.io/rpc/v0", "")
 	require.NoError(t, err)
-	require.EqualValues(t, 1598306400, GENESIS_TIMESTAMP)
+	require.EqualValues(t, 1598306400, GenesisTimestamp)
 }
 
 func TestCalibNet(t *testing.T) {
 	err := Initialize(context.Background(), "https://api.calibration.node.glif.io/rpc/v0", "")
 	require.NoError(t, err)
-	require.EqualValues(t, 1667326380, GENESIS_TIMESTAMP)
+	require.EqualValues(t, 1667326380, GenesisTimestamp)
 }
