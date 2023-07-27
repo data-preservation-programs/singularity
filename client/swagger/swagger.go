a798ce0ea8d0c5774be620ab9af66f80f5328494
Masih H. Derkani <m@derkani.org>
2023-07-27 17:34:13 +0100

    Use a fixed version of go-swagger in build

--- client/swagger/swagger.go
+++ client/swagger/swagger.go
@@ -1,12 +1,7 @@
 // Package swagger contains the client library code generated automatically from the swagger definitions.
 // The code present in http, models and operations must not be modified.
-// To run the source generation, swagger binary must be present in PATH.
-// To install the swagger binary, run:
-//
-//	go install github.com/go-swagger/go-swagger/cmd/swagger@latest
 //
 // For more information, see: https://github.com/go-swagger/go-swagger
 package swagger
 
-//go:generate swagger generate client -f ../../docs/swagger/swagger.json -t ../../ -c client/swagger/http -m client/swagger/models -a /client/swagger/operations
-//go:generate go mod tidy
+//go:generate go run github.com/go-swagger/go-swagger/cmd/swagger@v0.30.5 generate client -f ../../docs/swagger/swagger.json -t ../../ -c client/swagger/http -m client/swagger/models -a /client/swagger/operations --with-enum-ci
