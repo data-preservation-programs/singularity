f7764b9a497f909d7db8692538e9ba62a1fba161
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2023-08-09 14:45:47 -0700

    --really-do-it for dangerous command

--- cmd/admin/reset.go
+++ cmd/admin/reset.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"github.com/data-preservation-programs/singularity/cmd/cliutil"
 	"github.com/data-preservation-programs/singularity/database"
 	"github.com/data-preservation-programs/singularity/handler/admin"
 	"github.com/urfave/cli/v2"
@@ -9,7 +10,11 @@ import (
 var ResetCmd = &cli.Command{
 	Name:  "reset",
 	Usage: "Reset the database",
+	Flags: []cli.Flag{cliutil.ReallyDotItFlag},
 	Action: func(context *cli.Context) error {
+		if err := cliutil.HandleReallyDoIt(context); err != nil {
+			return err
+		}
 		db, closer, err := database.OpenFromCLI(context)
 		if err != nil {
 			return err
--- cmd/dataset/remove.go
+++ cmd/dataset/remove.go
@@ -1,6 +1,7 @@
 package dataset
 
 import (
+	"github.com/data-preservation-programs/singularity/cmd/cliutil"
 	"github.com/data-preservation-programs/singularity/database"
 	"github.com/data-preservation-programs/singularity/handler/dataset"
 	"github.com/urfave/cli/v2"
@@ -11,7 +12,13 @@ var RemoveDatasetCmd = &cli.Command{
 	Usage:       "Remove a specific dataset. This will not remove the CAR files.",
 	Description: "Important! If the dataset is large, this command will take some time to remove all relevant data.",
 	ArgsUsage:   "<dataset_name>",
+	Flags: []cli.Flag{
+		cliutil.ReallyDotItFlag,
+	},
 	Action: func(c *cli.Context) error {
+		if err := cliutil.HandleReallyDoIt(c); err != nil {
+			return err
+		}
 		db, closer, err := database.OpenFromCLI(c)
 		if err != nil {
 			return err
