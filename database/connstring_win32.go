0d0f4528cb69e6d20656c3a37a3c3ffeccb70001
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2023-08-12 00:23:24 -0700

    Enable 32bit tests (#196)
    Enable 32bit tests as stream-commp 32bit issue is now fixed.
    However, 32bit tests does not work on windows so let's just add another
    CI for 32-bit testing only on ubuntu

--- database/connstring_win32.go
+++ database/connstring_win32.go
@@ -1,4 +1,4 @@
-//go:build !cgo && !(windows && 386)
+//go:build windows && 386
 
 package database
 
@@ -6,7 +6,6 @@ import (
 	"io"
 	"strings"
 
-	"github.com/glebarez/sqlite"
 	"github.com/ipfs/go-log/v2"
 	"github.com/pkg/errors"
 	"gorm.io/driver/mysql"
@@ -22,38 +21,6 @@ func Open(connString string, config *gorm.Config) (*gorm.DB, io.Closer, error) {
 	var db *gorm.DB
 	var closer io.Closer
 	var err error
-	if strings.HasPrefix(connString, "sqlite:") {
-		logger.Info("Opening sqlite database (non-cgo version)")
-		db, err = gorm.Open(sqlite.Open(connString[7:]), config)
-		if err != nil {
-			return nil, nil, err
-		}
-
-		closer, err = db.DB()
-		if err != nil {
-			return nil, nil, err
-		}
-
-		err = db.Exec("PRAGMA foreign_keys = ON").Error
-		if err != nil {
-			closer.Close()
-			return nil, nil, err
-		}
-
-		err = db.Exec("PRAGMA busy_timeout = 50000").Error
-		if err != nil {
-			closer.Close()
-			return nil, nil, err
-		}
-
-		err = db.Exec("PRAGMA journal_mode = WAL").Error
-		if err != nil {
-			closer.Close()
-			return nil, nil, err
-		}
-
-		return db, closer, nil
-	}
 
 	if strings.HasPrefix(connString, "postgres:") {
 		logger.Info("Opening postgres database")
--- database/util.go
+++ database/util.go
@@ -85,29 +85,6 @@ func OpenFromCLI(c *cli.Context) (*gorm.DB, io.Closer, error) {
 	return OpenWithLogger(connString)
 }
 
-func OpenInMemory() (*gorm.DB, io.Closer, error) {
-	db, closer, err := OpenWithLogger("sqlite:file::memory:?cache=shared")
-	if err != nil {
-		logger.Error(err)
-		return nil, nil, err
-	}
-
-	err = DoRetry(func() error { return model.DropAll(db) })
-	if err != nil {
-		logger.Error(err)
-		closer.Close()
-		return nil, nil, err
-	}
-	err = DoRetry(func() error { return model.AutoMigrate(db) })
-	if err != nil {
-		logger.Error(err)
-		closer.Close()
-		return nil, nil, err
-	}
-
-	return db, closer, nil
-}
-
 func DropAll(db *gorm.DB) error {
 	return model.DropAll(db)
 }
