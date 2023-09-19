a1fcec1626550a823579f755353dafe75dcde4e6
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2023-09-19 13:54:36 -0700

    fix

--- pack/assembler_test.go
+++ pack/assembler_test.go
@@ -19,54 +19,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestAssembler_InaccessibleFile(t *testing.T) {
-	tmp := t.TempDir()
-	ctx := context.Background()
-	err := os.WriteFile(filepath.Join(tmp, "test.txt"), []byte("test"), 0000)
-	require.NoError(t, err)
-
-	stat, err := os.Stat(filepath.Join(tmp, "test.txt"))
-	require.NoError(t, err)
-
-	reader, err := storagesystem.NewRCloneHandler(ctx, model.Storage{
-		Type: "local",
-		Path: tmp,
-	})
-	require.NoError(t, err)
-
-	assembler := NewAssembler(context.Background(), reader, []model.FileRange{
-		{
-			Offset: 0,
-			Length: 4,
-			File: &model.File{
-				Path:             "test.txt",
-				Size:             4,
-				LastModifiedNano: stat.ModTime().UnixNano(),
-			},
-		},
-	}, false, false)
-	defer assembler.Close()
-
-	_, err = io.ReadAll(assembler)
-	require.Error(t, err)
-
-	assembler2 := NewAssembler(context.Background(), reader, []model.FileRange{
-		{
-			Offset: 0,
-			Length: 4,
-			File: &model.File{
-				Path:             "test.txt",
-				Size:             4,
-				LastModifiedNano: stat.ModTime().UnixNano(),
-			},
-		},
-	}, false, true)
-	defer assembler2.Close()
-
-	_, err = io.ReadAll(assembler2)
-	require.NoError(t, err)
-}
-
 func TestAssembler(t *testing.T) {
 	tmp := t.TempDir()
 	sizes := map[int]struct {
