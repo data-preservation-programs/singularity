eaebc6b9857639a12325133da4e1ff6c21cea433
Arkadiy Kukarkin <parkan@users.noreply.github.com>
2024-07-10 20:34:31 +0200

    [DRAFT] enable union backend (#439)
    this leverages built-in rclone features to allow backends that have one
    logical root (e.g. `internetarchive`) to seamlessly aggregate multiple
    paths into a single preparation
    
    example usage:
    ```
    RCLONE_CONFIG_IA_TYPE=internetarchive  singularity storage create union --upstreams 'ia:DuckandC1951 ia:0872_Angels_Flight_05_32_34_00' --path ''
    ```
    
    this creates an implicit, ephemeral remote of the `internetarchive` type
    called `ia` and aggregates paths specified by `--upstreams`

--- storagesystem/types.go
+++ storagesystem/types.go
@@ -45,6 +45,7 @@ import (
 	_ "github.com/rclone/rclone/backend/storj"
 	_ "github.com/rclone/rclone/backend/sugarsync"
 	_ "github.com/rclone/rclone/backend/swift"
+	_ "github.com/rclone/rclone/backend/union"
 	_ "github.com/rclone/rclone/backend/uptobox"
 	_ "github.com/rclone/rclone/backend/webdav"
 	_ "github.com/rclone/rclone/backend/yandex"
