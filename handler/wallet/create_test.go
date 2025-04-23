4a200318330911862a16e97f8a1ccd9346e359f7
Zach Fedor <zachfedor@gmail.com>
2025-04-23 16:07:50 -0400

    :wqRefactor create wallet handler for offline key generation

--- handler/wallet/create_test.go
+++ handler/wallet/create_test.go
@@ -3,7 +3,6 @@ package wallet
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/data-preservation-programs/singularity/handler/handlererror"
 	"github.com/data-preservation-programs/singularity/util"
@@ -16,8 +15,8 @@ func TestCreateHandler(t *testing.T) {
 	testutil.All(t, func(ctx context.Context, t *testing.T, db *gorm.DB) {
 		lotusClient := util.NewLotusClient("https://api.node.glif.io/rpc/v0", "")
 
-		t.Run("success", func(t *testing.T) {
-			w, err := Default.CreateHandler(ctx, db, lotusClient)
+		t.Run("success-secp256k1", func(t *testing.T) {
+			w, err := Default.CreateHandler(ctx, db, CreateRequest{ KeyType: KTSecp256k1.String() })
 			require.NoError(t, err)
 			require.NotEmpty(t, w.Address)
 			require.NotEmpty(t, w.PrivateKey)
@@ -28,12 +27,21 @@ func TestCreateHandler(t *testing.T) {
 			require.ErrorIs(t, err, handlererror.ErrDuplicateRecord)
 		})
 
-		t.Run("invalid response", func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-			defer cancel()
-			lotusClient := util.NewLotusClient("http://127.0.0.1", "")
-			_, err := Default.CreateHandler(ctx, db, lotusClient)
-			require.ErrorIs(t, err, handlererror.ErrInvalidParameter)
+		t.Run("success-bls", func(t *testing.T) {
+			w, err := Default.CreateHandler(ctx, db, CreateRequest{ KeyType: KTBLS.String() })
+			require.NoError(t, err)
+			require.NotEmpty(t, w.Address)
+			require.NotEmpty(t, w.PrivateKey)
+
+			_, err = Default.ImportHandler(ctx, db, lotusClient, ImportRequest{
+				PrivateKey: w.PrivateKey,
+			})
+			require.ErrorIs(t, err, handlererror.ErrDuplicateRecord)
+		})
+
+		t.Run("invalid-key-type", func(t *testing.T) {
+			_, err := Default.CreateHandler(ctx, db, CreateRequest{ KeyType: "invalid-type" })
+			require.Error(t, err)
 		})
 	})
 }
--- handler/wallet/interface.go
+++ handler/wallet/interface.go
@@ -20,7 +20,7 @@ type Handler interface {
 	CreateHandler(
 		ctx context.Context,
 		db *gorm.DB,
-		lotusClient jsonrpc.RPCClient,
+		request CreateRequest,
 	) (*model.Wallet, error)
 	DetachHandler(
 		ctx context.Context,
@@ -65,8 +65,8 @@ func (m *MockWallet) AttachHandler(ctx context.Context, db *gorm.DB, preparation
 	return args.Get(0).(*model.Preparation), args.Error(1)
 }
 
-func (m *MockWallet) CreateHandler(ctx context.Context, db *gorm.DB, lotusClient jsonrpc.RPCClient) (*model.Wallet, error) {
-	args := m.Called(ctx, db, lotusClient)
+func (m *MockWallet) CreateHandler(ctx context.Context, db *gorm.DB, request CreateRequest) (*model.Wallet, error) {
+	args := m.Called(ctx, db, request)
 	return args.Get(0).(*model.Wallet), args.Error(1)
 }
 
