805d9362e79bf0c3b2924115c72f6a74d5a52df3
Zach Fedor <zachfedor@gmail.com>
2025-06-05 23:17:43 -0400

    feat: better documentation of create cmd

--- cmd/wallet/create.go
+++ cmd/wallet/create.go
@@ -12,7 +12,26 @@ var CreateCmd = &cli.Command{
 	Name:      "create",
 	Usage:     "Create a new wallet",
 	ArgsUsage: "[type]",
-	Before:    cliutil.CheckNArgs,
+	Description: `Create a new Filecoin wallet using offline keypair generation.
+
+The wallet will be stored locally in the Singularity database and can be used for making deals and other operations. The private key is generated securely and stored encrypted.
+
+SUPPORTED KEY TYPES:
+  secp256k1    ECDSA using the secp256k1 curve (default, most common)
+  bls          BLS signature scheme (Boneh-Lynn-Shacham)
+
+EXAMPLES:
+  # Create a secp256k1 wallet (default)
+  singularity wallet create
+
+  # Create a secp256k1 wallet explicitly
+  singularity wallet create secp256k1
+
+  # Create a BLS wallet
+  singularity wallet create bls
+
+The newly created wallet address and other details will be displayed upon successful creation.`,
+	Before: cliutil.CheckNArgs,
 	Action: func(c *cli.Context) error {
 		db, closer, err := database.OpenFromCLI(c)
 		if err != nil {
