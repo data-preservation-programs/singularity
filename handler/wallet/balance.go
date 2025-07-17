803cfafd69be0fb53e3cf1a02c31ef51535ebfbf
Anjor Kanekar <anjor@umd.edu>
2025-07-17 06:50:53 +0100

    Update handler/wallet/balance.go
    Co-authored-by: Adrian Lanzafame <adrianlanzafame92@gmail.com>

--- handler/wallet/balance.go
+++ handler/wallet/balance.go
@@ -115,10 +115,6 @@ func getDatacapBalance(ctx context.Context, lotusClient jsonrpc.RPCClient, addr 
 		return 0, nil
 	}
 
-	// If result is empty or "null", client has no datacap
-	if result == "" || result == "null" {
-		return 0, nil
-	}
 
 	// Parse the datacap balance string
 	datacap, err := strconv.ParseInt(result, 10, 64)
