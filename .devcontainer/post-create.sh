341f0f7bc560287dd5ccfa001696f644f3948b81
Arkadiy Kukarkin <arkadiy@archive.org>
2025-10-27 12:43:08 +0000

    fix another devcontainer oopsie, hooks should no longer run

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -15,7 +15,7 @@ mkdir -p /home/vscode/.local/share/pg/pgdata /home/vscode/.local/share/pg /home/
 # Initialize Postgres
 if [ ! -f "/home/vscode/.local/share/pg/pgdata/PG_VERSION" ]; then
   echo "Initializing Postgres..."
-  /usr/lib/postgresql/16/bin/initdb -D /home/vscode/.local/share/pg/pgdata --auth trust --auth-local trust --auth-host trust
+  /usr/lib/postgresql/16/bin/initdb -D /home/vscode/.local/share/pg/pgdata --auth trust --auth-local trust --auth-host trust --encoding=UTF8 --locale=C.UTF-8
   echo "listen_addresses='localhost'" >> /home/vscode/.local/share/pg/pgdata/postgresql.conf
   {
     echo 'host    all all 127.0.0.1/32     trust'
