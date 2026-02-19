17525062233cc12082d1b47b348d4cb0ca232eb1
anjor <anjor@umd.edu>
2026-02-19 12:34:51 +0000

    Add PDP scheduling API boundary interfaces

---
+++ service/dealpusher/pdp_api_test.go
@@ -0,0 +1,25 @@
+package dealpusher
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPDPSchedulingConfigValidate(t *testing.T) {
+	t.Run("valid", func(t *testing.T) {
+		cfg := PDPSchedulingConfig{
+			BatchSize:         100,
+			GasLimit:          5000000,
+			ConfirmationDepth: 5,
+			PollingInterval:   30 * time.Second,
+		}
+		require.NoError(t, cfg.Validate())
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		cfg := PDPSchedulingConfig{}
+		require.Error(t, cfg.Validate())
+	})
+}
