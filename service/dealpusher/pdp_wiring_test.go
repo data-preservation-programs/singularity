b084cf436e3d4b4564518d474bb2adc45eaa7d83
anjor <anjor@umd.edu>
2026-02-20 14:39:15 +0000

    Fix staticcheck unused PDP wiring test mocks

--- service/dealpusher/pdp_wiring_test.go
+++ service/dealpusher/pdp_wiring_test.go
@@ -13,22 +13,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type noopPDPProofSetManager struct{}
-
-func (noopPDPProofSetManager) EnsureProofSet(_ context.Context, _ model.Wallet, _ string) (uint64, error) {
-	return 1, nil
-}
-
-func (noopPDPProofSetManager) QueueAddRoots(_ context.Context, _ uint64, _ []cid.Cid, _ PDPSchedulingConfig) (*PDPQueuedTx, error) {
-	return &PDPQueuedTx{Hash: "0x1"}, nil
-}
-
-type noopPDPTransactionConfirmer struct{}
-
-func (noopPDPTransactionConfirmer) WaitForConfirmations(_ context.Context, txHash string, _ uint64, _ time.Duration) (*PDPTransactionReceipt, error) {
-	return &PDPTransactionReceipt{Hash: txHash}, nil
-}
-
 type fixedWalletChooser struct {
 	wallet model.Wallet
 }
